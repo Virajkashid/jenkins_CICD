dd559351b1175bf1c8859ed8c02c11fc9a4a644d
viraj <viraj@gmil.com>
2024-04-08 00:23:31 +0530

    --- Auto Git Commit ---

--- studentapp.ui.groovy
+++ studentapp.ui.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('pull') {
             steps {
-                git branch: 'main', credentialsId: 'ssh', url: 'git branch: 'main ', url: 'https://github.com/Virajkashid/jenkins_CICD.git'
+                git branch: 'main', credentialsId: 'ssh',  ', url: 'https://github.com/Virajkashid/jenkins_CICD.git'
                 echo 'pull'
             }
         }
