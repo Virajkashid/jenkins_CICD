89a05a1fdf2471cea18b1d57c5d01956197e1810
viraj <viraj@gmil.com>
2024-04-07 22:35:43 +0530

    --- Auto Git Commit ---

---
+++ studentapp.ui.groovy
@@ -0,0 +1,34 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Pull') {
+            steps {
+                git branch: 'main', changelog: false, credentialsId: 'ssh-key', poll: false, url: 'https://github.com/chetansomkuwar254/jenkins-b2'
+                echo 'Here we are pullin our project repository'
+            }
+        }
+        stage('Build') {
+            steps {
+                sh '/opt/apache-maven-3.9.6/bin/mvn clean package'
+                echo 'Here we doing build operation'
+            }
+        }
+        stage('Test') {
+            steps {
+                echo 'Here we are testing '
+            }
+        }
+        stage('Quality Test') {
+            steps {
+                echo 'Here we are testing the quality'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploy Done'
+            }
+        
+        }
+    }
+}
